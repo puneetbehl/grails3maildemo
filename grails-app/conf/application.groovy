a40c98c680e5b34ddc6af7d753a2a20fdffcb680
Puneet Behl <puneetbehl@yahoo.com>
2015-04-14 22:23:11 +0530

    Changed sender email

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -2,8 +2,8 @@ grails {
    mail {
      host = "smtp.gmail.com"
      port = 465
-     username = "aditya.linkshare@gmail.com"
-     password = "linkshare@123"
+     username = "senderemail@gmail.com"
+     password = "password"
      props = ["mail.smtp.auth":"true",
               "mail.smtp.socketFactory.port":"465",
               "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
